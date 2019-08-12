743d142e9e86826038851992a176798e40df5d43
Pu Wang <pu.wang@smartline.com.au>
2019-08-12 13:22:12 +0800

    Support to pick up a car with a ticket

--- src/main/java/ParkingLotService.java
+++ src/main/java/ParkingLotService.java
@@ -1,7 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParkingLotService {
 
+  private Map<Ticket, Car> cars = new HashMap<>();
+
   public Ticket park(Car car) {
-    return new Ticket();
+    Ticket ticket = new Ticket();
+    cars.put(ticket, car);
+    return ticket;
   }
 
+  public Car pickUp(Ticket ticket) {
+    return cars.get(ticket);
+  }
 }
--- src/test/java/ParkingLotServiceTest.java
+++ src/test/java/ParkingLotServiceTest.java
@@ -1,16 +1,32 @@
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class ParkingLotServiceTest {
 
+  private ParkingLotService parkingLotService;
+
+  @Before
+  public void setUp() {
+   parkingLotService = new ParkingLotService();
+  }
+
   @Test
   public void shouldReturnATicketWhenParkingACar() {
-    ParkingLotService parkingLotService = new ParkingLotService();
-
     Car car = new Car();
     Ticket ticket = parkingLotService.park(car);
 
     assertNotNull(ticket);
+  }
+
+  @Test
+  public void shouldPickUpACarWithATicket() {
+    Car expected = new Car();
+    Ticket ticket = parkingLotService.park(expected);
+    Car actual = parkingLotService.pickUp(ticket);
+
+    assertEquals(expected, actual);
   }
 }
